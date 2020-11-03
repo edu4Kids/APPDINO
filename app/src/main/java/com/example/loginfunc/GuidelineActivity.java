04577e82828ddc43e1d6c6f9b1d278973833ccc1
hyebinkang <khb1029@naver.com>
2020-11-04 01:42:51 +0900

    guide

---
+++ app/src/main/java/com/example/loginfunc/GuidelineActivity.java
@@ -0,0 +1,31 @@
+package com.example.loginfunc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class GuidelineActivity extends AppCompatActivity {
+    private FragmentPagerAdapter fragmentPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpager);
+
+        //
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        fragmentPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+}
